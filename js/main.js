b05f1f783d5b3ef00e33bfce4a8f8c20642bf571
Cobaltium <matthewferderber@gmail.com>
2014-08-29 12:03:21 -0400

    added image started work on responsive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+	var filename = window.location.hash.substring(1);
+	if(filename == ""){
 	$(".main-content").load("content/introduction.html #content");
+} else {
+	$(".main-content").load("content/" + filename +".html #content");
+}
 
 	$(".main-content").css('display', "block");
 	$(document).css('height', '100%');
@@ -13,25 +18,31 @@ $(document).ready(function() {
 	$("ul li a").click(function(e) {
 
 		var filename = $(this).attr('href').substring(1) + ".html";
+		animate(filename);
+	});
+
+});
+
+function animate(filename) {
+	if ($(document).width() > 1024) {
 		$(".main-content").animate({
 			top: '-150%'
 		}, 300, function() {
+			$(".main-content").load("content/" + filename + " #content");
 			$(".main-content").css('display', "none");
-			$(".main-content").load("content/" + filename + " #content")
 			$(".main-content").css('top', '100%');
-			//$(".main-content").css('display', "inline-block");
+			$(".main-content").css('display', "inline-block");
 			$(document).css('height', '100%');
 			$(".main-content").animate({
 				top: '0'
 			}, 300, function() {
 				$(document).css('height', 'auto');
-				$(".main-content").animate({
-					'boxShadow': "10px 10px 10px rgb(200,200,200)"
-				}, 300, function() {
-
-				});
+				// $(".main-content").animate({
+				// 	'boxShadow': "10px 10px 10px rgb(200,200,200)"
+				// }, 300);
 			});
 		});
-	});
-
-});
+	} else {
+		$(".main-content").load("content/" + filename + " #content");
+	}
+}
