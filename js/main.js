6380de737e1536f08c82ba7632f5f885ea9389de
Cobaltium <matthewferderber@gmail.com>
2014-09-03 11:16:44 -0400

    formatted pages

--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
 $(document).ready(function() {
-	var filename = window.location.hash.substring(1);
+  var filename = window.location.hash.substring(1);
 
-	$('.main-content').css('display', 'none');
+  $('.main-content').css('display', 'none');
 
-	if (filename == "") {
-		$("#newContent").load("content/introduction.html #content", null, loadContent);
-	} else {
-		$("#newContent").load("content/" + filename + ".html #content",null, loadContent);
-	}
+  if (filename == "") {
+    $("#newContent").load("content/introduction.html #content", null, loadContent);
+  } else {
+    $("#newContent").load("content/" + filename + ".html #content", null, loadContent);
+  }
 
-	$("ul li a").click(function(e) {
+  $("ul li a").click(function(e) {
 
-		var filename = $(this).attr('href').substring(1) + ".html";
-		$("#newContent").load("content/" + filename + " #content", null, loadContent);
-	});
+    var filename = $(this).attr('href').substring(1) + ".html";
+    $("#newContent").load("content/" + filename + " #content", null, loadContent);
+  });
 
 });
 
 function loadContent() {
-	if (isDesktop()) {
-		animate();
-	} else {
-		$('.main-content').css('display', 'block');
-		$('.main-content').html($('#newContent').html());
+  if (isDesktop()) {
+    animate();
+  } else {
+    $('.main-content').css('display', 'block');
+    $('.main-content').html($('#newContent').html());
 
-	}
+  }
 }
 
 function isDesktop() {
 
-	if ($(document).width() > 1024) {
-		return true;
-	} else {
-		return false;
-	}
+  if ($(document).width() > 1024) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function animate() {
-	$(".main-content").animate({
-		top: '-150%'
-	}, 300, function() {
-		$('.main-content').css('display', 'none');
-		$('.main-content').html($('#newContent').html());
-		$('.main-content').css('top', '100%');
-		$('.main-content').css('display', "inline-block");
-		scrollPaneBack();
-	});
+  $(".main-content").animate({
+    top: '-150%'
+  }, 300, function() {
+    $('.main-content').css('display', 'none');
+    $('.main-content').html($('#newContent').html());
+    $('.main-content').css('top', '100%');
+    $('.main-content').css('display', "inline-block");
+    scrollPaneBack();
+  });
 }
 
 function scrollPaneBack() {
-	$(document).css('height', '100%');
-	$(".main-content").animate({
-		top: '0'
-	}, 300, function() {
-		$(document).css('height', 'auto');
-	});
+  $(document).css('height', '100%');
+  $(".main-content").animate({
+    top: '0'
+  }, 300, function() {
+    $(document).css('height', 'auto');
+  });
 }
